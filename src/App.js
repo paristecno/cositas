f0baa6b10633935e81a08471bbbd22d81f533fe6
paristecno <paristecnoargentina@gmail.com>
2024-05-18 22:20:05 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> esto es !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn tu mama en concha
-        </a>
-      </header>
+    <div>
+      <h1>hola</h1>
     </div>
   );
 }
