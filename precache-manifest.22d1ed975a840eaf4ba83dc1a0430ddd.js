c9149a941006f6e2a3aecca834212790bac70a33
paristecno <paristecno@users.noreply.github.com>
2024-05-21 02:51:34 +0000

    deploy: 14839f5bad911f21fabe7f6940c83f2f62a9012d

---
+++ precache-manifest.22d1ed975a840eaf4ba83dc1a0430ddd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614fddff574483e856eb86d387b1fe5b",
+    "url": "/cositas/index.html"
+  },
+  {
+    "revision": "2d04bd18f268305c3f3a",
+    "url": "/cositas/static/css/main.49ca8e89.chunk.css"
+  },
+  {
+    "revision": "16493cb902558cb903f7",
+    "url": "/cositas/static/js/2.cc7c1aaa.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/cositas/static/js/2.cc7c1aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d04bd18f268305c3f3a",
+    "url": "/cositas/static/js/main.39ce3eed.chunk.js"
+  },
+  {
+    "revision": "0eef642559342cdc3c68",
+    "url": "/cositas/static/js/runtime-main.79535c4f.js"
+  },
+  {
+    "revision": "d8b453197e639e5d2985f962063ec24a",
+    "url": "/cositas/static/media/banerCelu.d8b45319.png"
+  },
+  {
+    "revision": "49fc32736e41d3ab3d36df683f5c604c",
+    "url": "/cositas/static/media/bannerPC.49fc3273.png"
+  }
+]);
