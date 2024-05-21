32a8e2e45938e1132b493d8bcc05331b5ef9fa44
paristecno <paristecnoargentina@gmail.com>
2024-05-20 22:11:37 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Landing/Home/Home";
 import Detalle from './components/Detail/Detalle/Detalle';
+//hola
 
 function App() {
   return (
